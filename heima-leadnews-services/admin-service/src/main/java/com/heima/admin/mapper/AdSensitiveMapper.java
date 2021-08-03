0ae1c4b47bd8a96ed7ab569c55d59d2d62ed77de
benlee <892915332@qq.com>
2021-08-03 21:41:29 +0800

    实现敏感词的增删改查

---
+++ heima-leadnews-services/admin-service/src/main/java/com/heima/admin/mapper/AdSensitiveMapper.java
@@ -0,0 +1,5 @@
+package com.heima.admin.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.admin.pojos.AdSensitive;
+public interface AdSensitiveMapper extends BaseMapper<AdSensitive> {
+}
